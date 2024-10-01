497bcf23ec4de615b717b1c60808964aa65c5987
HuseyinK0r0glu <korogluh27@gmail.com>
2024-10-01 15:12:34 +0300

    another permutation implementation added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        permutation("abc","");
+        String str = "abct";
+        permutation(str,0,str.length()-1);
 
     }
 
+    /*
     public static void permutation(String str,String prefix){
         if(str.length() == 0){
             System.out.println(prefix);
@@ -15,6 +17,33 @@ public class Main {
                 permutation(rem,prefix + str.charAt(i));
             }
         }
+    }
+
+    */
+
+    public static void permutation(String str,int left,int right){
+
+        if(left == right){
+            System.out.println(str);
+        }else{
+            for(int i = left;i<=right;i++){
+                str = swap(str,left,i);
+                permutation(str,left+1,right);
+                str = swap(str,left,i);
+            }
+        }
+
+    }
+
+    public static String swap(String str,int i,int j){
+
+        char[] charArray = str.toCharArray();
+
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+
+        return String.valueOf(charArray);
     }
 
 }
